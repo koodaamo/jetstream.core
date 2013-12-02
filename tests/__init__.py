aeb7ae692ec9089ec06393845cb677987606d196
Petri Savolainen <petri.savolainen@iki.fi>
2013-12-02 10:20:11 +0200

    fix test suite

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,11 +1,5 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import unittest
 
-import os, unittest
-
-#def testsuite(loader, standard_tests, pattern):
-#    # top level directory cached on loader instance
-#    this_dir = os.path.dirname(__file__)
-#    package_tests = loader.discover(start_dir=this_dir, pattern=pattern)
-#    standard_tests.addTests(package_tests)
-#    return standard_tests
+def test_suite():
+    suite = unittest.TestLoader().discover(start_dir=".")
+    return suite
